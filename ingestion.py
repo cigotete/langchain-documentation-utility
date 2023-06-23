105030a3738611957473ecd8ac66d8f10a18be45
Camilo Figueroa <cigotete@gmail.com>
2023-06-23 16:14:09 -0500

    Loading documentation located inside langchain-docs directory.

---
+++ ingestion.py
@@ -0,0 +1,9 @@
+import os
+from langchain.document_loaders import ReadTheDocsLoader
+
+def ingest_docs() -> None:
+    loader = ReadTheDocsLoader(path="langchain-docs/langchain.readthedocs.io/en/latest", encoding="utf-8")
+    raw_documents = loader.load()
+
+if __name__ == "__main__":
+    ingest_docs()
