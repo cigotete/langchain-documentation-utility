ccfd3e0987dc464f05e9cc54653617f7e6bccac2
Camilo Figueroa <cigotete@gmail.com>
2023-06-23 17:07:03 -0500

    Splitting documents into chunks.

--- ingestion.py
+++ ingestion.py
@@ -1,9 +1,16 @@
 import os
 from langchain.document_loaders import ReadTheDocsLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def ingest_docs() -> None:
     loader = ReadTheDocsLoader(path="langchain-docs/langchain.readthedocs.io/en/latest", encoding="utf-8")
     raw_documents = loader.load()
+    print(f"loaded {len(raw_documents) }documents")
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=100, separators=["\n\n", "\n", " ", ""]
+    )
+    documents = text_splitter.split_documents(documents=raw_documents)
+    print(f"Splitted into {len(documents)} chunks")
 
 if __name__ == "__main__":
     ingest_docs()
